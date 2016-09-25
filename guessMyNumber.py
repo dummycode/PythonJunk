3144fb2a8da3ccf504f48e7f65408dba928aace5
Henry Harris <henrythomasharris@gmail.com>
2016-09-25 19:04:18 -0400

    Fun lil game

---
+++ guessMyNumber.py
@@ -0,0 +1,21 @@
+import random
+
+secretNumber = random.randint(1, 100) # random number in range [1, 100]
+print('I am thinking of a number between 1 and 100...')
+
+# Get player's guess
+for numOfGuesses in range(1, 7):
+    print('Take a guess: ', end='')
+    guess = int(input())
+    if (guess > secretNumber):
+        print('Lower...')
+    elif(guess < secretNumber):
+        print('Higher...')
+    else:
+        break
+
+if (guess == secretNumber):
+    print('Congrats, you successfully guessed in ' + str(numOfGuesses) + ' guesses')
+else:
+    print('You idiot, the number I was thinking of was ' + str(secretNumber))
+
