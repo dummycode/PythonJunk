848b38a01ed77f820e296364cdf312ceac81d635
Henry Harris <henrythomasharris@gmail.com>
2016-12-21 13:43:44 -0500

    Improved the phone number finder

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -22,3 +22,11 @@ print('800-2E4-7628 is not a phone number')
 print(isPhoneNumber('800-2E4-7628'))
 print('Yoshi is not a phone number')
 print(isPhoneNumber('Yoshi'))
+
+# Check this blob of text for any
+blob = 'Call me at 415-001-6783 tomorrow. 678-903-3245 is my office phone.'
+for i in range(len(blob)):
+    chunk = blob[i:i+12]
+    if (isPhoneNumber(chunk)):
+        print('Phone number found: ' + chunk)
+
