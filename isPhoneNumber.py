d3089d141ea1ff59a0b883016a6decaaa4ee48aa
Henry Harris <henrythomasharris@gmail.com>
2016-12-26 14:20:04 -0500

    Greatly improved phone number matching

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,20 +1,8 @@
+import re
+
 def isPhoneNumber(text):
-    if (len(text) != 12):
-        return False
-    for i in range(0, 3):
-        if (not text[i].isdecimal()):
-            return False
-    if (text[3] != '-'):
-        return False
-    for i in range(4, 7):
-        if (not text[i].isdecimal()):
-            return False
-    if (text[7] != '-'):
-        return False
-    for i in range(8, 11):
-        if (not text[i].isdecimal()):
-            return False
-    return True
+    phoneNumberRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
+    return (not phoneNumberRegex.search(text) == None)
 
 print('720-256-7244 is a phone number')
 print(isPhoneNumber('720-256-7244'))
